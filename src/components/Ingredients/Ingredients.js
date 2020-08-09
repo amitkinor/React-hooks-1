86f01a36d85186e2b96e7744834de07172c43f9f
amitkinor <amit.kinor@gmail.com>
2020-08-09 16:58:23 +0300

    useCallback, useEffect

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,19 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
 import Search from './Search';
 
-function Ingredients() {
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  const addIngredientHandler = ingredient => {
+    fetch(
+      'https://react-hooks-cd19d.firebaseio.com//ingredients.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(ingredient),
+        headers: {'Content-Type': 'application/json'}
+      })
+      .then((response) => response.json())
+      .then(responseData => {
+        setUserIngredients(prevIngredients => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient }
+        ])
+      })
+  }   ;
+
+  useEffect(() => console.log('rendering ingredients', userIngredients),
+    [userIngredients]);
+
+  const removeItemHandler = (id) => {
+    fetch(
+      `https://react-hooks-cd19d.firebaseio.com//ingredients/${id}.json`,
+      {
+        method: 'DELETE',
+        
+      })
+      .then(responseData => {
+        setUserIngredients((prevIngredients) => 
+        prevIngredients.filter( ing => ing.id !== id)
+        )
+      });
+  };
+
+  const ingsUpdateFilterHandler = 
+    useCallback(  
+      ((filteredIngs) => {
+        setUserIngredients(filteredIngs);
+      })
+    ,[]);
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
-        <Search />
-        {/* Need to add list here! */}
+        <Search updateFilterHandler={ingsUpdateFilterHandler}/>
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeItemHandler} />
       </section>
     </div>
   );
-}
+};
 
 export default Ingredients;
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,19 +1,58 @@
-import React from 'react';
+import React , { useState, useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+
 
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
+
+   const { updateFilterHandler } = props;
+   const [enteredFilter, setEntered] = useState('');
+   const inputRef = useRef();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if(enteredFilter === inputRef.current.value){
+        const querry = enteredFilter.length === 0
+        ? ''
+        : `?orderBy="title"&equalTo="${enteredFilter}"`;
+      fetch('https://react-hooks-cd19d.firebaseio.com//ingredients.json' + querry)
+        .then((response) => response.json())
+        .then(responseData => {
+          const loadedIngredients = [];
+          for (const key in responseData) {
+            loadedIngredients.push({
+              id: key,
+              title: responseData[key].title,
+              amount: responseData[key].amount,
+            });
+          }
+         updateFilterHandler(loadedIngredients);
+        }); 
+      }    
+    }, 500);
+    return () => {
+      clearTimeout(timer);
+    };
+    
+  },[enteredFilter, updateFilterHandler, inputRef]);
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input ref={inputRef} type="text" value={enteredFilter} onChange={(event) => setEntered(event.target.value)}/>
         </div>
       </Card>
     </section>
   );
 });
+
+Search.propTypes = {
+  //ingredients: PropTypes.array.isRequired,
+  updateFilterHandler: PropTypes.func.isRequired,
+}
 
 export default Search;
